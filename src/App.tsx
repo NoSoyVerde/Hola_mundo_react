a12425fc7cc3fb4770f4c71b43adea7557ebd070
NoSoyVerde <hectorkotor@gmail.com>
2025-09-16 14:02:25 +0200

    Quitado el titulo

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,6 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h2>Hector</h2>
       <div className="card">
         <button onClick={handleClick}>
           count is {count}
